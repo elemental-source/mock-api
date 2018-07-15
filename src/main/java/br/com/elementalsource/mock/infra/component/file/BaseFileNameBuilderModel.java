c1a0b4cd21355a30825be07fdbb96d00ebb111fc
André Formento <andreformento.sc@gmail.com>
2018-07-15 19:31:40 -0300

    Update version

--- src/main/java/br/com/elementalsource/mock/infra/component/file/BaseFileNameBuilderModel.java
+++ src/main/java/br/com/elementalsource/mock/infra/component/file/BaseFileNameBuilderModel.java
@@ -8,12 +8,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Component("BaseFileNameBuilderModel")
 public class BaseFileNameBuilderModel extends BaseFileNameBuilderBase implements BaseFileNameBuilder {
@@ -26,15 +20,6 @@ public class BaseFileNameBuilderModel extends BaseFileNameBuilderBase implements
         final String fileBase = fileProperty.getFileBase();
         final File file = new File(fileBase);
         LOGGER.info("Base path to files fileBase={}, exists?{}, path={}", fileBase, file.exists(), file.getAbsoluteFile());
-
-        try {
-            final List<Path> collect = Files.list(file.toPath()).collect(Collectors.toList());
-            LOGGER.info("count = {}", collect.size());
-            collect.stream().map(Path::toString).forEach(LOGGER::info);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
     }
 
 }
